afe15b9d9fa371ec4236b66dace871dc747d9d2f
lmeline <loise13@hotmail.fr>
2025-01-07 14:13:29 +0100

    test ajout door43

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
-
+import Door43LoginButton from '../services/ConnectWithDoor43';
 const Home = () => {
     return (
         <div>
+            <Door43LoginButton/>
             <Navigation/>
             <p> Veuillez vous connecter </p>
         </div>
